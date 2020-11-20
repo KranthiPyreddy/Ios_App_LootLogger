78b787b1c2a3347236a9269990e1748fb05a3fd6
kranthi pyreddy <kra330@gmail.com>
2020-11-20 13:15:00 -0500

    Lab 4 Part B: UITableView, UITableViewController and Editing Mode

--- LootLogger/ItemStore.swift
+++ LootLogger/ItemStore.swift
@@ -11,11 +11,34 @@ class ItemStore {
 
         return newItem
     }
+    
+    //implement a new method to remove a specific item.
+    //“Removing an item from the store
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    //Reordering items within the store
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
     //Populating the ItemStore with Item instances
-    init() {
+    /*init() {
         for _ in 0..<5 {
             createItem()
         }
-    }
+    } */
 }
 
